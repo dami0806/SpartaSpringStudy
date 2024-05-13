68956820150a5e6b96c1d4b51e1b18f8f016b1e9
dami0806 <dami0804@naver.com>
2024-05-13 19:27:14 +0900

    Http를 객체로 처리하는 방법

---
+++ spring-mvc/src/main/java/org/sparta/springmvc/request/Star.java
@@ -0,0 +1,12 @@
+package org.sparta.springmvc.request;
+
+
+public class Star {
+    String name;
+    int age;
+
+    public Star(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
